36eb91764b6d5c738c52ec77b655638aa26a1477
JasonReese2 <jasonearlreese@gmail.com>
2023-10-24 22:58:51 -0400

    Changed database config from sqlite to mysql

--- WardrobeApp/website/__init__.py
+++ WardrobeApp/website/__init__.py
@@ -4,13 +4,14 @@ from os import path
 from flask_login import LoginManager
 
 db = SQLAlchemy()
-DB_NAME = "database.db"
+DB_NAME = "WardrobeDatabase"
+
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'secretKey'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://root:password@localhost/{DB_NAME}'
     db.init_app(app)
 
 
